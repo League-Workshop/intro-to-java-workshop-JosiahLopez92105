71ee78b2c75ed19c989d4d0841abec68a3126147
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:28:41 -0700

    Workshop robot

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,26 +6,32 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot gouko=new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		gouko.setSpeed(100);
 		// 5. Set the pen width to 5
-
+gouko.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for(   int i=0; i<4 ; i++ ) {
 			// 7. Set the pen color to random
-	
+	gouko.setRandomPenColor();
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+gouko.turn(90);}
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
+		gouko.penDown();
+		for(   int i=0; i<4 ; i++ ) {
+			gouko.setSpeed(100);
+			gouko.turn(90);
+			gouko.move(100);}
+			
 		
 		
 	}
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,7 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot gouko = new Robot();
+	gouko.setSpeed(100);
+	gouko.penDown();
+	gouko.setPenColor(Color.red);
+	for(   int i=0; i<4 ; i++ ) {
+		gouko.turn(90);
+		gouko.move(100);
+		
 	
+	}
+
 		
 		
 	}
